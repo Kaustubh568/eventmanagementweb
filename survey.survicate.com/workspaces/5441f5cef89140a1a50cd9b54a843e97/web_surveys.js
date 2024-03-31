04606613a3ba02332986c244aba2cab8ca8ef59b
Kaustubh chandel <austubh@student.iul.ac.in>
2024-03-31 23:56:52 +0530

    Add files via upload

---
+++ survey.survicate.com/workspaces/5441f5cef89140a1a50cd9b54a843e97/web_surveys.js
@@ -0,0 +1,75 @@
+var _sTrackingAlreadyPresent = (typeof window._svd !== 'undefined' && typeof window._svc !== 'undefined');var _svc = window._svc || {};var _svd = window._svd || {};_svc.workspaceKey = _svc.workspaceKey || '5441f5cef89140a1a50cd9b54a843e97';_svd.flags = _svd.flags || {"async_consumers":true};_svd.surveys = _svd.surveys || [{"id":"ef6af9c41c476d8a","activated":true,"name":"Web - NPS Website Survey","type":"WidgetSurvey","points":[{"settings":{"text_on_the_left":"Not likely at all","text_on_the_right":"Extremely likely","first_range_tag":"NPS-Detractor","second_range_tag":"NPS-Passive","third_range_tag":"NPS-Promoter","first_range_goto_id":null,"second_range_goto_id":null,"third_range_goto_id":null,"logic":[]},"id":978261,"type":"SurveyNps","answer_type":"nps","content":"How likely are you to recommend WeddingWire to a friend who’s getting married? 📋","description":"","max_path":2},{"settings":{"logic":[],"user_tag":"","mandatory":false,"next_survey_point_id":null},"id":978262,"type":"SurveyQuestion","answer_type":"text","content":"Please explain the reason for your rating.","description":"","max_path":1,"answers":[{"id":2726016,"type":null,"user_tag":null,"next_survey_point_id":null}]},{"settings":{"link":"","text":"","user_tag":"","twitter_link":"","facebook_link":"","linkedin_link":"","open_new_card":true,"next_survey_point_id":-1,"timeout_display_count":5},"id":978277,"type":"SurveyCta","answer_type":"empty","content":"Thank you!","description":"","max_path":0}],"project_id":109889,"integrations":{},"links":[{"link":"pre10.weddingwire.in","visible":true,"link_type":"simple","regexp_text_pattern":"^pre10\\.weddingwire\\.in"},{"link":"communitypre10.weddingwire.in","visible":true,"link_type":"simple","regexp_text_pattern":"^communitypre10\\.weddingwire\\.in"}],"audiences_ids":[243431],"provider":null,"theme_id":417315,"global_tags":null,"global_close_tags":"","settings":{"launch":{"end_at":null,"start_at":null,"responses_limit":null},"overlay":null,"messages":{"tooltip":"Please answer our short survey","submit_text":"Submit","answer_required_text":"","dropdown_placeholder":""},"recurring":false,"close_icon":"bottom","show_close":true,"display_all":true,"display_per":"visit","appear_method":"delayed","display_delay":10,"has_responses":true,"place_to_show":"left","show_minimize":false,"display_scroll":80,"run_minimalized":false,"survey_language":"","recurring_period":86400,"show_progress_bar":false,"close_on_urlchange":false,"display_percentage":3,"display_not_engaged":false,"run_minimalized_mobile":false,"hide_footer":true,"languages":null}},{"id":"61f0681a15c15b54","activated":true,"name":"Weddingwire.in NPS Vendors","type":"WidgetSurvey","points":[{"settings":{"text_on_the_left":"Not likely","text_on_the_right":"Very likely","first_range_tag":"NPS-Detractor","second_range_tag":"NPS-Passive","third_range_tag":"NPS-Promoter","first_range_goto_id":null,"second_range_goto_id":null,"third_range_goto_id":null,"logic":[]},"id":1511696,"type":"SurveyNps","answer_type":"nps","content":"How likely are you to recommend WeddingWire.in to other wedding professionals?","description":"","max_path":1},{"settings":{"user_tag":"","mandatory":false,"next_survey_point_id":-1},"id":1511697,"type":"SurveyQuestion","answer_type":"text","content":"What is the primary reason for your answer?","description":"","max_path":0,"answers":[{"id":4320947,"type":null,"user_tag":null,"next_survey_point_id":null}]}],"project_id":109889,"integrations":{},"links":[{"link":"weddingwire.in/vendor/campus","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/vendor/campus"},{"link":"weddingwire.in/emp-AdminSolicitudes.php","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-AdminSolicitudes.php"},{"link":"weddingwire.in/emp-AdminReviews.php","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-AdminReviews.php"},{"link":"weddingwire.in/emp-Modif.php","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-Modif.php"},{"link":"weddingwire.in/emp-ModifMapa.php","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-ModifMapa.php"},{"link":"weddingwire.in/emp-AdminFaqs.php","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-AdminFaqs.php"},{"link":"weddingwire.in/emp-AdminPromociones.php","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-AdminPromociones.php"},{"link":"weddingwire.in/emp-ModifGaleria.php","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-ModifGaleria.php"},{"link":"weddingwire.in/emp-AdminVideos.php","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-AdminVideos.php"},{"link":"weddingwire.in/emp-AdminCronicasListLanding.php","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-AdminCronicasListLanding.php"},{"link":"weddingwire.in/emp-AdminEventos.php","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-AdminEventos.php"},{"link":"weddingwire.in/emp-AdminEndorsement.php","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-AdminEndorsement.php"},{"link":"weddingwire.in/emp-AdminOwners.php","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-AdminOwners.php"},{"link":"weddingwire.in/emp-AdminRedesSociales.php","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-AdminRedesSociales.php"},{"link":"weddingwire.in/emp-Sello.php","visible":true,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-Sello.php"},{"link":"weddingwire.in/emp-Menu.php","visible":false,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-Menu.php"},{"link":"weddingwire.in/emp-AdminRecibos.php","visible":false,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-AdminRecibos.php"},{"link":"weddingwire.in/emp-AdminNotificaciones.php","visible":false,"link_type":"regexp","regexp_text_pattern":"weddingwire.in/emp-AdminNotificaciones.php"}],"audiences_ids":[243431],"provider":null,"theme_id":417315,"global_tags":null,"global_close_tags":null,"settings":{"has_responses":true,"appear_method":"delayed","close_icon":"bottom","close_on_urlchange":false,"survey_language":"","display_all":false,"display_delay":10,"display_not_engaged":false,"display_per":"visit","display_percentage":10,"display_scroll":80,"hide_footer":true,"launch":{"start_at":null,"end_at":null,"responses_limit":null},"messages":{"submit_text":"Submit","tooltip":"Please answer our short survey","answer_required_text":"Answer required","dropdown_placeholder":"Type or select an option"},"overlay":"light","place_to_show":"right","recurring":true,"recurring_period":7776000,"run_minimalized":false,"run_minimalized_mobile":false,"show_close":true,"show_minimize":false,"show_progress_bar":true,"languages":null}}];_svd.themes = _svd.themes || [{"settings":{"company_logo":"","personalization":false,"short_text_message":"","avatar_image":""},"id":417315,"color_scheme":{"question":"#29292a","answer":"#29292a","button":"#FB1B26","background":"#ecf0f1","progress_bar":"#FB1B26"},"type":"MicroTheme"},{"settings":{"company_logo":"","personalization":false,"short_text_message":"","avatar_image":""},"id":417315,"color_scheme":{"question":"#29292a","answer":"#29292a","button":"#FB1B26","background":"#ecf0f1","progress_bar":"#FB1B26"},"type":"MicroTheme"}];_svd.permissions = _svd.permissions || {"targeting_javascript_api":true};_svd.audiences = _svd.audiences || [{"id":243431,"filters":[{"exclude":false,"parameters":{"count":1},"name":"visit_frequency"}]}];_svd.integrations = _svd.integrations || [];_svd.installing = _svd.installing || false;_svd.translations = _svd.translations || null;// Generated at: 2023\u002D05\u002D11\u002016\u003A32\u003A05
+(function () {
+  if (_sTrackingAlreadyPresent) {
+    return;
+  }
+
+  if (document && document.head) {
+    var linkElement = document.createElement('link');
+    linkElement.href = 'https\u003A\/\/surveys\u002Dstatic.survicate.com/fonts/fonts.css';
+    linkElement.rel = 'stylesheet';
+    linkElement.type = 'text/css';
+    document.head.appendChild(linkElement);
+  }
+
+  var isIE = window.navigator.userAgent.indexOf('MSIE') !== -1 || window.navigator.userAgent.match(/Trident.*rv\:11\./);
+  var isSafari = window.navigator.vendor && window.navigator.vendor.indexOf('Apple') > -1 && window.navigator.userAgent && window.navigator.userAgent.indexOf('CriOS') == -1 && window.navigator.userAgent.indexOf('FxiOS') == -1;
+
+  var isOldSafari = false;
+  if (isSafari && window.navigator.userAgent) {
+    var match = window.navigator.userAgent.match(/Version.([0-9]+)\./);
+    if (match && match.length === 2) {
+      if (parseInt(match[1]) < 11) {
+        isOldSafari = true;
+      }
+    }
+  }
+  var isOldEdge = window.navigator.userAgent.match(/Edge\/(15\.15|18\.18)/);
+
+  var coreUrls = ['https://surveys-static.survicate.com/widget_core-18.4.0.js'];
+  var e = document.getElementsByTagName('script')[0];
+
+  if (isIE || isOldSafari || isOldEdge) {
+    var css = 'h1 { width: 75%; height: 50%; overflow: auto; margin: auto; position: absolute; top: 0; left: 0; bottom: 0; right: 0; text-align: center; font-family: sans-serif; }'
+    var head = document.head || document.getElementsByTagName('head')[0];
+    var style = document.createElement('style');
+    var message = 'Your browser doesn’t meet our security standards. Please update your browser to answer this survey.'
+    var body = document.querySelector('body');
+    var splash = document.createElement('div');
+    var survey = document.querySelector('.survicate-survey');
+
+    if(!head || !survey) {
+      return;
+    }
+
+    head.appendChild(style);
+
+    style.type = 'text/css';
+
+    if (style.styleSheet){
+      // This is required for IE8 and below.
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+
+    splash.classList.add('splash-screen');
+    splash.innerHTML = '<h1>' + message + '</h1>';
+
+    if(!body) {
+      return;
+    }
+
+    body.appendChild(splash);
+
+    return;
+  }
+
+  for (var i = 0; i < coreUrls.length; i++) {
+    var s = document.createElement('script');
+    s.setAttribute('crossorigin', 'anonymous');
+    s.src = coreUrls[i];
+    s.async = true;
+    e.parentNode.insertBefore(s, e);
+  }
+})();
